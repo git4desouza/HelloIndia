cc5cf17a3d0d2561624bd268e0533f7a88934917
git4desouza <marchiyusaruldesouza@gmail.com>
2019-11-30 19:27:31 +0530

    Initial Commit

---
+++ src/com/dx/client/Hello.java
@@ -0,0 +1,12 @@
+package com.dx.client;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello India");
+
+	}
+
+}
